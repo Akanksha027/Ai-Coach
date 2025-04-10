d5ca08b33be295b7c39ef0e81f3114b463cd5746
Akanksha Singh <akanskahsingh0085@gmail.com>
2025-04-10 12:19:39 +0530

    ab chal jana bhaiii

--- app/layout.js
+++ app/layout.js
@@ -1,41 +1,48 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "../components/ui/theme-provider";
-import Header from "../components/Header";
 import { ClerkProvider } from "@clerk/nextjs";
-import { Toaster } from "@/components/ui/sonner";
+import { Toaster } from "sonner";
+import Header from "@/components/header";
+import { ThemeProvider } from "@/components/theme-provider";
+import { dark } from "@clerk/themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Sensai-AI",
-  description: "BU AI expertt!!!",
+  title: "AI Career Coach",
+  description: "",
 };
 
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider
       appearance={{
-        baseTheme: "dark",
+        baseTheme: dark,
       }}
     >
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="dark"
-        enableSystem
-        disableTransitionOnChange
-      >
-        {/*header*/}
-        <Header />
-        <main className={`min-h-screen ${inter.className}`}>{children}</main>
-        <Toaster richColors />
-        {/* footer */}
-        <footer className="bg-muted/50 py-12">
-          <div className="container mx-auto px-4 text-center text-gray-200">
-            <p>Made with 💙 and geniousness by Akanksha</p>
-          </div>
-        </footer>
-      </ThemeProvider>
+      <html lang="en" suppressHydrationWarning>
+        <head>
+          <link rel="icon" href="/logo.png" sizes="any" />
+        </head>
+        <body className={`${inter.className}`}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Header />
+            <main className="min-h-screen">{children}</main>
+            <Toaster richColors />
+
+            <footer className="bg-muted/50 py-12">
+              <div className="container mx-auto px-4 text-center text-gray-200">
+                <p>Made with 💗 by RoadsideCoder</p>
+              </div>
+            </footer>
+          </ThemeProvider>
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
