5aeb6d3048997045d801f3ddac6a1d606ca47caf
Akanksha Singh <akanskahsingh0085@gmail.com>
2025-04-10 13:23:32 +0530

    ab please chal jana bas

--- app/(main)/layout.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Mainlayout = ({children}) => {
-  return (
-    <div className="container mx-auto mt-24 mb-20" > {children}</div>
-  )
-}
-
-export default Mainlayout;  
