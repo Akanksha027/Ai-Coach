1caa3bf8ccc87231f7859e7a2002745be7d8eb25
Akanksha Singh <akanskahsingh0085@gmail.com>
2025-04-10 09:42:48 +0530

    chaljaaaa

--- app/layout.js
+++ app/layout.js
@@ -14,32 +14,28 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider appearance={{
-      baseTheme: "dark",
-    }} >
-      <html lang="en" suppressHydrationWarning>
-        <body
-          className={`${inter.className}`}
-        >
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {/*header*/}
-            <Header />
-            <main className="min-h-screen">{children}</main>
-            <Toaster richColors/>
-            {/* footer*/}
-            <footer className="bg-muted/50 py-12">
-              <div className="container mx-auto px-4 text-center text-gray-200">
-                <p>Made with 💙 and geniousness by Akanksha</p>
-              </div>
-            </footer>
-          </ThemeProvider>
-        </body>
-      </html>
+    <ClerkProvider
+      appearance={{
+        baseTheme: "dark",
+      }}
+    >
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {/*header*/}
+        <Header />
+        <main className={`min-h-screen ${inter.className}`}>{children}</main>
+        <Toaster richColors />
+        {/* footer */}
+        <footer className="bg-muted/50 py-12">
+          <div className="container mx-auto px-4 text-center text-gray-200">
+            <p>Made with 💙 and geniousness by Akanksha</p>
+          </div>
+        </footer>
+      </ThemeProvider>
     </ClerkProvider>
   );
 }
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,6 +1,5 @@
 // pages/_document.jsx
 import { Html, Head, Main, NextScript } from 'next/document';
-import { Html as CustomHtml } from 'next/document'; // ❌ Don't do this in other files!
 
 export default function Document() {
   return (
